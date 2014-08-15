da1dd2cdca731d0388554b655de7476de5810699
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-08-15 18:04:24 +0530

    Initial Commit

---
+++ TimesTwo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Guwanjith Tennekoon on 8/15/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
